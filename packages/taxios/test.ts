77d65e10d41a5b0056df7da51414a20021ce3f81
truschery <truschery.work@gmail.com>
2025-07-04 14:31:17 +0300

    added proxy

--- packages/taxios/test.ts
+++ packages/taxios/test.ts
@@ -1,4 +1,16 @@
 import {ProxyMiddleware, TaxiosClient} from "./src";
+import { ProxyAgent, fetch } from "undici"
+import tunnel from 'tunnel'
+
+
+
+const agent1 = tunnel.httpOverHttps({
+    proxy: {
+            host: 'http://80.243.140.54', // IP или хост прокси
+            port: 59100,
+            proxyAuth: 'pavikforcebussines:bQDkmAabC2'
+        }
+})
 
 
 const client = new TaxiosClient({
@@ -19,18 +31,34 @@ client.use(ProxyMiddleware, 'request:before', {
 })
 
 const main = async () => {
-    client.get('https://httpbin.org/ip', {
-            protocol: 'http', // или 'https' в зависимости от прокси
-            host: '80.243.140.54', // IP или хост прокси
-            port: 59100,
-            auth: {
-                username: 'pavikforcebussines',
-                password: 'bQDkmAabC2'
-            },
-        }).then(result => {
-        console.log(result)
+    //46.3.132.179:59100:pavikforcebussines:bQDkmAabC2
+    const agent = new ProxyAgent('http://pavikforcebussines:bQDkmAabC2@46.3.132.179:59100')
 
+
+    // {
+    //         protocol: 'http', // или 'https' в зависимости от прокси
+    //         host: '80.243.140.54', // IP или хост прокси
+    //         port: 59100,
+    //         auth: {
+    //             username: 'pavikforcebussines',
+    //             password: 'bQDkmAabC2'
+    //         },
+    //     }
+
+
+    fetch('https://httpbin.org/ip', {
+        // dispatcher: agent,
+    }).then(async result => {
+        console.log(await result.text());
+        
     })
+
+    // client.get('https://httpbin.org/ip', {
+    //     httpsAgent: agent1,
+    // }).then(result => {
+    //     console.log(result)
+
+    // })
 
     return 0
 }
